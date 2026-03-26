040f136750207e6fb804248a6b5c8316f997db7f
FADelto <dimka.shkolovoy90@gmail.com>
2026-03-26 15:18:12 +0300

    реализовал тест-кейсы для PostsService

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -13,10 +13,25 @@ describe('PostsService', () => {
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    // act
+    const result = postsService.create(post);
+
+    // assert
+    expect(result.text).toEqual('Mocked post');
+    expect(result.id).toEqual('2');
+    expect(result.date).toBeDefined();
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    // arrange
+    const created = postsService.create(post);
+
+    // act
+    const found = postsService.find(created.id);
+
+    // assert
+    expect(found).toBeDefined();
+    expect(found?.id).toEqual(created.id);
+    expect(found?.text).toEqual(created.text);
   });
 });
